e1d017082e1c9fb352bf2103666cb9980ed78e9b
Clémence <clemvoegele@gmail.com>
2022-01-03 19:15:40 +0100

    Mise au propre Classe Hole

--- src/Hole.hpp
+++ src/Hole.hpp
@@ -3,12 +3,7 @@
 #include "Tile.hpp"
 
 class Hole : public Tile {
-private:
-    // std::string m_imageTex = "assets/textures/hole.png"; // TODO: est-ce qu'on a besoin de le définir ici parce que le constructeur prend déjà en paramètre une texture qui et créée ailleurs
-
 public:
     Hole(GLfloat posX, GLfloat posY, GLuint& texture)
-        : Tile(posX, posY, texture)
-    {
-    }
+        : Tile(posX, posY, texture) {}
 };
